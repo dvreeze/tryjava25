bde3e5daca95d0dd13a9ebb6ef9487d9cb3081de
Chris de Vreeze <chris.de.vreeze@caiway.net>
2026-04-21 23:53:46 +0200

    Added FileIOExample (and jextract)

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
 
-Trying out Java 25
-==================
+# Trying out Java 25
 
 Trying out Java 25 JDK APIs (not necessarily introduced in Java 25), such as Stream Gatherers.
+
+## FFM
+
+Some programs experimented with the FFM API (Java 22+). For some of these programs the "jextract" tool was used.
+For regular Linux platforms: download "jextract", and extract (with "tar xvfz") the "gz" file under the jextract folder.
+
+To generate native bindings in Java for stdio.h, use the following command or something similar (for Linux):
+
+```bash
+# From the root of the project, assuming jextract has been installed as described above
+./jextract/jextract-25/bin/jextract \
+  --include-dir /usr/include \
+  --output src/main/java \
+  --target-package eu.cdevreeze.tryjava25.generated \
+  "<stdio.h>"
+```
+
+For more information on how to use "jextract", see [jextract](https://github.com/openjdk/jextract/blob/master/doc/GUIDE.md). We can also find many examples there, to
+get a better feel for the "jextract" tool.
+
+Other interesting links on "jextract" are [dev.java on jextract](https://dev.java/learn/jvm/tools/complementary/jextract/)
+and [jdk.java.net/jextract](https://jdk.java.net/jextract/).
---
+++ src/main/java/eu/cdevreeze/tryjava25/ffm/console/FileIOExample.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2025-2026 Chris de Vreeze
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package eu.cdevreeze.tryjava25.ffm.console;
+
+import module java.base;
+import com.google.common.base.Preconditions;
+
+import static eu.cdevreeze.tryjava25.generated.stdio_h.*;
+
+/**
+ * Simple program performing native file I/O, using the {@link java.lang.foreign} API and output of the "jextract" tool.
+ * It counts the number of lines, using native calls. Of course, this is just a toy application using FFM.
+ * <p>
+ * See for example <a href="https://www.baeldung.com/java-foreign-memory-access">Java Foreign Memory Access</a>
+ * and <a href="https://dev.java/learn/jvm/tools/complementary/jextract/">jextract</a>.
+ * <p>
+ * To run this program, enable native access with "--enable-native-access=ALL-UNNAMED".
+ *
+ * @author Chris de Vreeze
+ */
+public class FileIOExample {
+
+    static void main(String... args) {
+        Objects.checkIndex(0, args.length);
+        Path path = Path.of(args[0]);
+
+        try (Arena arena = Arena.ofConfined()) {
+            try {
+                int lineCount = getLineCount(path, arena);
+                System.out.printf("Line count: %d%n", lineCount);
+            } catch (RuntimeException e) {
+                System.err.printf("Could not compute line count. Exception message: %s%n", e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * Returns the line count in the given file, or throws an exception if the line count could not be obtained.
+     * Note how the Arena dynamically allocates memory in a safe way. It is still native "C memory",
+     * yet safely managed through the FFM API.
+     * <p>
+     * Note how we are both in the "C world" and the "Java world". The "C world" is visible in the C function
+     * contracts and types, and the use of native memory and therefore of FFM MemorySegment objects everywhere,
+     * even for "native null pointers". The "Java world" is visible in the use of Java as a language, and
+     * the use of Java language features to safely manage native memory.
+     * <p>
+     * To avoid making mistakes against the proper use of native memory in the native function calls,
+     * we need to have a good look at the native function contracts in the C header files that declare those functions.
+     */
+    public static int getLineCount(Path path, Arena arena) {
+        MemorySegment file = null;
+        try {
+            // Returns a pointer to FILE
+            // See https://en.cppreference.com/c/io/fopen
+            file = fopen(arena.allocateFrom(path.toString()), arena.allocateFrom("r"));
+
+            // Note the use of "native memory NPE" checks, through a comparison of the MemorySegment against MemorySegment.NULL
+            // If we forget to do such checks where needed we invite core dumps!
+            Preconditions.checkState(file != null && !file.equals(MemorySegment.NULL));
+
+            int numberOfLines = 0;
+            int bufferLength = 10_000;
+            // Pointer to (1 byte) char
+            MemorySegment line = arena.allocate(bufferLength);
+
+            // The fgets call returns a pointer to (1 byte) char
+            // See https://en.cppreference.com/c/io/fgets
+            while (!fgets(line, bufferLength, file).equals(MemorySegment.NULL)) {
+                if (line.getString(0).contains("\n")) { // Inefficient to turn this into a Java String
+                    numberOfLines++;
+                }
+            }
+
+            return numberOfLines;
+        } finally {
+            // Again the "native memory NPE check", or else we get a core dump if the actual file does not exist
+            if (file != null && !file.equals(MemorySegment.NULL)) {
+                // See https://en.cppreference.com/c/io/fclose
+                fclose(file);
+            }
+        }
+    }
+}
